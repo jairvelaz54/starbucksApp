83a771ae3979a27faeb7e29cecc95ed01c82386e
Jair Velazquez Reyes <jairreyes689@gmail.com>
2024-09-12 21:23:13 -0600

    agregando cambios

--- lib/drinkCard.dart
+++ lib/drinkCard.dart
@@ -1,52 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:starbucks_app/colors.dart';
 import 'package:starbucks_app/drink_screen.dart';
+import 'dart:math' as math;
 
-class Drinkcard  extends StatelessWidget{
-  DrinkScreen drink;
-  Drinkcard(this.drink);
+class DrinkCard extends StatelessWidget {
+  final DrinkScreen drink;
+  final double pageOffset;
+  final int index;
+  double animation = 0;
+  double animate = 0;
+  double columnAnimation = 0;
+  double rotate = 0;
+
+  DrinkCard(this.drink, this.pageOffset, this.index);
+
   @override
   Widget build(BuildContext context) {
-    Size size= MediaQuery.of(context).size;
-    double cardWidth= size.width-60;
-    double cardHeight= size.height*.55;
+    Size size = MediaQuery.of(context).size;
+    double cardWidth = size.width - 60;
+    double cardHeight = size.height * .55;
+    double count = 0;
+    double page = pageOffset;
+    rotate = index - pageOffset;
+
+    // Calcular la animación basada en el offset de la página
+    while (page > 1) {
+      page--;
+      count++;
+    }
+    animation = Curves.easeOutBack.transform(page);
+    animate = 80 * (count + animation);  // Ajustar para reducir el desplazamiento
+    columnAnimation = 40 * (index - pageOffset);  // Ajuste del desplazamiento de la columna
 
     return Container(
       child: Stack(
-        clipBehavior: Clip.none, children: <Widget>[
+        clipBehavior: Clip.none,
+        children: <Widget>[
           buildTopText(),
-          buildBackgroundImage(cardWidth, cardHeight,size),
+          buildBackgroundImage(cardWidth, cardHeight, size),
+          buildAboveCard(cardWidth, cardHeight, size),
+          buildCupImage(size),
+          buildBlurImage(cardWidth, size),
+          buildSmallImage(size),
+          buildTopImage(cardWidth, cardHeight, size),
         ],
       ),
     );
   }
-  
+
   Widget buildTopText() {
     return Padding(
       padding: const EdgeInsets.only(top: 30.0),
       child: Row(
         children: <Widget>[
-          SizedBox(
-            width: 20,
-          ),
+          SizedBox(width: 20),
           Text(
             drink.name,
             style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 50,
-                color: drink.lightColor),
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+              color: drink.lightColor,
+            ),
           ),
           Text(
             drink.conName,
             style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 50,
-                color: drink.dartColor),
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+              color: drink.dartColor,
+            ),
           ),
         ],
       ),
     );
   }
-  
+
   Widget buildBackgroundImage(double cardWidth, double cardHeight, Size size) {
     return Positioned(
       width: cardWidth,
@@ -62,6 +90,118 @@ class Drinkcard  extends StatelessWidget{
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildAboveCard(double cardWidth, double cardHeight, Size size) {
+    return Positioned(
+      width: cardWidth,
+      height: cardHeight,
+      bottom: size.height * .15,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        decoration: BoxDecoration(
+          color: drink.dartColor.withOpacity(.50),
+        ),
+        padding: EdgeInsets.all(30),
+        child: Transform.translate(
+          offset: Offset(-columnAnimation, 0),  // Reducción del desplazamiento horizontal
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Frappuccino',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                drink.description,
+                style: TextStyle(color: Colors.white70, fontSize: 18),
+              ),
+              Spacer(),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(width: 5),
+                  Image.asset('images/cup_L.png'),
+                  SizedBox(width: 5),
+                  Image.asset('images/cup_M.png'),
+                  SizedBox(width: 5),
+                  Image.asset('images/cup_s.png'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  color: mAppGreen,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      SizedBox(width: 20),
+                      Text(
+                        '\$',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        '4.',
+                        style: TextStyle(fontSize: 19, color: Colors.white),
+                      ),
+                      Text(
+                        '70',
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCupImage(Size size) {
+    return Positioned(
+      bottom: 20,
+      right: -size.width * .1 / 2 + 10,  
+      child: Transform.rotate(
+        angle: math.pi / 14 * rotate,
+        child: Image.asset(drink.cupImage, height: size.height * .55 - 15),
+      ),
+    );
+  }
+
+  Widget buildBlurImage(double cardWidth, Size size) {
+    return Positioned(
+      right: cardWidth / 2 - 60 + animate,  
+      bottom: size.height * .10,
+      child: Image.asset(drink.imageBlur),
+    );
+  }
+
+  Widget buildSmallImage(Size size) {
+    return Positioned(
+      right: -10 + animate,
+      top: size.height * .3,
+      child: Image.asset(drink.imageSmall),
+    );
+  }
+
+  Widget buildTopImage(double cardWidth, double cardHeight, Size size) {
+    return Positioned(
+      left: cardWidth / 4 - animate,  
+      bottom: size.height * .15 + cardHeight - 25,
+      child: Image.asset(drink.imageTop),
     );
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,66 +8,133 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  PageController? pageController;
+  double pageOffset = 0;
+  AnimationController? controller;
+  Animation<double>? animation;
+
+  @override
+  void initState() {
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 800));
+    animation = CurvedAnimation(parent: controller!, curve: Curves.easeOutBack);
+    pageController = PageController(viewportFraction: 0.8, initialPage: 0);
+    pageController?.addListener(() {
+      setState(() {
+        pageOffset = pageController?.page ?? 0;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    pageController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size size= MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-          child: Stack(
-        children: <Widget>[buildTooBar(),
-        buldLogo(size),
-        buildPager(size),
-        ],
-      )),
+        child: Stack(
+          children: <Widget>[
+            buildToolBar(),
+            buildLogo(size),
+            buildPager(size),
+            buildPageIndicator(),
+          ],
+        ),
+      ),
     );
   }
 
-  Widget buildTooBar() {
+  Widget buildToolBar() {
     return Padding(
       padding: const EdgeInsets.only(top: 20.0),
       child: Row(
         children: <Widget>[
-          SizedBox(
-            width: 20,
-          ),
-          Image.asset(
-            "images/location.png",
-            width: 30,
-            height: 30,
+          SizedBox(width: 20),
+          AnimatedBuilder(
+            animation: animation!,
+            builder: (context, snapshot) {
+              return Transform.translate(
+                offset: Offset(-200 * (1 - animation!.value), 0),
+                child: Image.asset(
+                  "images/location.png",
+                  width: 30,
+                  height: 30,
+                ),
+              );
+            },
           ),
           Spacer(),
-          Image.asset(
-            "images/drawer.png",
-            width: 30,
-            height: 30,
+          AnimatedBuilder(
+            animation: animation!,
+            builder: (context, snapshot) {
+              return Transform.translate(
+                offset: Offset(200 * (1 - animation!.value), 0),
+                child: Image.asset(
+                  "images/drawer.png",
+                  width: 30,
+                  height: 30,
+                ),
+              );
+            },
           ),
-          SizedBox(
-            width: 20,
-          )
+          SizedBox(width: 20),
         ],
       ),
     );
   }
-  
-  Widget buldLogo(Size size) {
+
+  Widget buildLogo(Size size) {
     return Positioned(
       top: 10,
-      right: size.width/2-25,
-      child: Image.asset("images/logo.png", width: 50, height: 50,),
+      right: size.width / 2 - 25,
+      child: AnimatedBuilder(
+        animation: animation!,
+        builder: (context, snapshot) {
+          return Transform(
+            transform: Matrix4.identity()
+              ..translate(0.0, size.height / 2 * (1 - animation!.value))
+              ..scale(1 + (1 - animation!.value)),
+            origin: Offset(25, 25),
+            child: InkWell(
+              onTap: () => controller!.isCompleted ? controller!.reverse() : controller!.forward(),
+              child: Image.asset("images/logo.png", width: 50, height: 50),
+            ),
+          );
+        },
+      ),
     );
   }
-  
- Widget buildPager(Size size) {
+
+  Widget buildPager(Size size) {
     return Container(
       margin: EdgeInsets.only(top: 70),
-      height: size.height-50,
-      child: PageView.builder(itemCount: getDrinks().length, itemBuilder: (context,index)=>Drinkcard(getDrinks()[index])),
+      height: size.height - 50,
+      child: AnimatedBuilder(
+        animation: animation!,
+        builder: (context, snapshot) {
+          return Transform.translate(
+            offset: Offset(400 * (1 - animation!.value), 0),
+            child: PageView.builder(
+              controller: pageController,
+              itemCount: getDrinks().length,
+              itemBuilder: (context, index) => DrinkCard(getDrinks()[index], pageOffset, index),
+            ),
+          );
+        },
+      ),
     );
   }
+
   List<DrinkScreen> getDrinks() {
-    List<DrinkScreen> list = [];
-    list.add(DrinkScreen(
+    return [
+      DrinkScreen(
         'Tirami',
         'Sù',
         'images/blur_image.png',
@@ -77,8 +144,9 @@ class _HomeScreenState extends State<HomeScreen> {
         'images/cup.png',
         'then top with whipped cream and mocha drizzle to bring you endless \njava joy',
         mBrownLight,
-        mBrown));
-    list.add(DrinkScreen(
+        mBrown,
+      ),
+      DrinkScreen(
         'Green',
         'Tea',
         'images/green_image.png',
@@ -88,8 +156,9 @@ class _HomeScreenState extends State<HomeScreen> {
         'images/green_tea_cup.png',
         'milk and ice and top it with sweetened whipped cream to give you \na delicious boost\nof energy.',
         greenLight,
-        greenDark));
-    list.add(DrinkScreen(
+        greenDark,
+      ),
+      DrinkScreen(
         'Triple',
         'Mocha',
         'images/mocha_image.png',
@@ -99,7 +168,44 @@ class _HomeScreenState extends State<HomeScreen> {
         'images/mocha_cup.png',
         'layers of whipped cream that’s infused with cold brew, white chocolate mocha and dark \ncaramel.',
         mBrownLight,
-        mBrown));
-    return list;
+        mBrown,
+      ),
+    ];
+  }
+
+  Widget buildPageIndicator() {
+    return AnimatedBuilder(
+      animation: controller!,
+      builder: (context, snapshot) {
+        return Positioned(
+          bottom: 10,
+          left: 10,
+          child: Opacity(
+            opacity: controller!.value,
+            child: Row(
+              children: List.generate(getDrinks().length, (index) => buildContainer(index)),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildContainer(int index) {
+    double animate = (pageOffset - index).abs();
+    double size = 10;
+    Color color = Colors.grey;
+
+    if (animate <= 1) {
+      size = 10 + 10 * (1 - animate);
+      color = ColorTween(begin: Colors.grey, end: mAppGreen).transform(1 - animate)!;
+    }
+
+    return Container(
+      margin: EdgeInsets.all(4),
+      height: size,
+      width: size,
+      decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(20)),
+    );
   }
 }
